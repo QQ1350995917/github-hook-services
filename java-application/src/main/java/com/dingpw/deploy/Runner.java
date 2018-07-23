f7d3d3329314918c6129847c57092fe4cf5c0987
丁朋伟 <dingpengwei@syswin.com>
2018-07-23 17:16:02 +0800

    test

--- java-application/src/main/java/com/dingpw/deploy/Runner.java
+++ java-application/src/main/java/com/dingpw/deploy/Runner.java
@@ -9,6 +9,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -59,13 +61,14 @@ public class Runner extends AbstractVerticle {
             Process process = run.exec(new String[]{"/bin/sh", "-c", cmd});
             InputStream in = process.getInputStream();
             BufferedReader bs = new BufferedReader(new InputStreamReader(in));
-//            List<String> list = new ArrayList<String>();
-//            String result = null;
-//            while ((result = bs.readLine()) != null) {
-//                System.out.println("job result [" + result + "]");
-//                list.add(result);
-//            }
+            List<String> list = new ArrayList<>();
+            String result;
+            while ((result = bs.readLine()) != null) {
+                System.out.println("job result [" + result + "]");
+                list.add(result);
+            }
             in.close();
+            bs.close();
             process.destroy();
         } catch (IOException e) {
             e.printStackTrace();
